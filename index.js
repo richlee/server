55f2a860de8914a0e17d5b6321ce92d2bd86c386
Rich <richalee@pm.me>
2021-08-18 21:18:49 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express'); // common js modules for node.js instead of "import express from 'express'" as node didn't supportES2015
+
+const app = express(); 
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'Dickie'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+
+
