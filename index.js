538559334329a0895c4dd5ec514bb17be8790fbe
Richard Lee <richard.lee@bjss.com>
2021-08-31 22:19:17 +0100

    WIP to flip - Google auth underway

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require('express'); // common js modules for node.js instead of "import express from 'express'" as node didn't supportES2015
+require('./services/passport');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ SeeYa: 'Dickie'});
-});
+require('./routes/authRoutes')(app); // require returns a function, which is immediately invoked, passing 'app' to attach the two route handlers
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
