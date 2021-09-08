537736316b852f23aae9f0794dbd3a243fdc0569
Rich <richalee@pm.me>
2021-09-09 00:17:19 +0100

    WIP

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express'); // common js modules for node.js instead of "import express from 'express'" as node didn't supportES2015
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app); // require returns a function, which is immediately invoked, passing 'app' to attach the two route handlers
 
